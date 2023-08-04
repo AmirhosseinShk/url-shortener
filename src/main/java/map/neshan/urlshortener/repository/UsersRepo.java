e48f1d4becf89dcd28b1427a9d3e9336e30d3f7a
Amirhossein <amirshk76@outlook.com>
2023-08-04 18:43:26 +0330

    add users model and add user repo and fix an issue on repository definition

--- src/main/java/map/neshan/urlshortener/repository/UsersRepo.java
+++ src/main/java/map/neshan/urlshortener/repository/UsersRepo.java
@@ -1,9 +1,9 @@
 package map.neshan.urlshortener.repository;
 
-import map.neshan.urlshortener.models.UrlsStatus;
+import map.neshan.urlshortener.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UrlShortenerRepo extends JpaRepository<UrlsStatus, Integer> {
+public interface UsersRepo extends JpaRepository<Users, Integer> {
 }
